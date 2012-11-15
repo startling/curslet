7c574e571875c7df02847cc670b802b509079325
startling <tdixon51793@gmail.com>
2012-11-15 06:23:05 -0500

    a Color instance.

--- UI/Curslet/Ncurses.hs
+++ UI/Curslet/Ncurses.hs
@@ -2,25 +2,34 @@ module UI.Curslet.Ncurses where
 -- base
 import Data.Bits ((.|.))
 -- curslet
-import UI.Curslet.Attributes
+import UI.Curslet.Class
 import UI.Curslet.Ncurses.Internal.Header
+import UI.Curslet.Ncurses.Internal.Bindings
 
 data Attr = Attr
-  { _bold      :: Bool       
+  { _bold      :: Bool
   , _blink     :: Bool
-  , _underline :: Bool }
+  , _underline :: Bool
+  , _fg        :: Maybe System
+  , _bg        :: Maybe System }
   deriving (Eq, Show)
 
 bitfield :: Attr -> Attr_t
-bitfield (Attr b l u) = foldr (.|.) 0
+bitfield (Attr b l u f g) = foldr (.|.) 0
   [ if b then a_bold else 0
   , if l then a_blink else 0
   , if u then a_underline else 0 ]
 
 instance Style Attr where
-  bold fn (Attr b l u) = fn b <&> \b -> Attr b l u
+  bold fn a = fn (_bold a) <&> \b -> a { _bold = b }
     where (<&>) = flip fmap
-  blink fn (Attr b l u) = fn l <&> \l -> Attr b l u
+  blink fn a = fn (_blink a) <&> \b -> a { _blink = b }
     where (<&>) = flip fmap
-  underline fn (Attr b l u) = fn u <&> \u -> Attr b l u
+  underline fn a = fn (_underline a) <&> \u -> a { _underline = u }
+    where (<&>) = flip fmap
+
+instance Color Attr where
+  fg fn a = fn (_fg a) <&> \f -> a { _fg = f }
+    where (<&>) = flip fmap
+  bg fn a = fn (_bg a) <&> \f -> a { _bg = f }
     where (<&>) = flip fmap
