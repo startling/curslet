cc3741e2676b4f4c2aae1c41ce471c587ed29a64
startling <tdixon51793@gmail.com>
2012-11-15 07:12:27 -0500

    some more bindings and thin wrappers.

--- UI/Curslet/Ncurses/Internal/Bindings.hs
+++ UI/Curslet/Ncurses/Internal/Bindings.hs
@@ -5,6 +5,9 @@ module UI.Curslet.Ncurses.Internal.Bindings where
 import Foreign
 import Foreign.C.Types
 import Control.Applicative
+import Data.Char (chr)
+-- curslet
+import UI.Curslet.Ncurses.Internal.Header
 
 -- | A dummy type for our pointers to point to.
 data Win_t
@@ -20,8 +23,43 @@ foreign import ccall "ncurses.h initscr"
 foreign import ccall "ncurses.h has_colors"
   c_has_colors :: IO CInt
 
+foreign import ccall "ncurses.h can_change_color"
+  c_can_change_color :: IO CInt
+
+foreign import ccall "ncurses.h color_content"
+  c_color_content_ :: CShort -> Ptr CShort -> Ptr CShort
+    -> Ptr CShort -> IO CInt
+
+-- | A thin wrapper around 'c_color_content_'; takes a CShort as
+-- a color index and gives you the red, green, and blue values
+-- as a triplet of CShorts.
+c_color_content :: CShort -> IO (Maybe (CShort, CShort, CShort))
+c_color_content n = alloca $ \a -> alloca $ \b -> alloca $ \c -> do
+  r <- c_color_content_ n a b c
+  if r == c_ok
+    then fmap Just $ (,,) <$> peek a <*> peek b <*> peek c
+    else return Nothing
+
+foreign import ccall "ncurses.h pair_content"
+  c_pair_content_ :: CShort -> Ptr CShort -> Ptr CShort -> IO CInt
+
+-- | A thing wrapper around 'c_pair_content'; takes a CShort
+-- as a pair index and returns the colors contained therein.
+c_pair_content :: CShort -> IO (Maybe (CShort, CShort))
+c_pair_content n = alloca $ \a -> alloca $ \b -> do
+  r <- c_pair_content_ n a b
+  if r == c_ok
+    then fmap Just $ (,) <$> peek a <*> peek b
+    else return Nothing
+
 foreign import ccall "ncurses.h start_color"
   c_start_color :: IO CInt
+
+foreign import ccall "ncurses.h COLORS"
+  c_colors :: IO CInt
+
+foreign import ccall "ncurses.h COLOR_PAIRS"
+  c_color_pairs :: IO CInt
 
 foreign import ccall "ncurses.h endwin"
   c_endwin :: IO CInt
@@ -32,7 +70,16 @@ foreign import ccall "ncurses.h COLOR_PAIR"
 foreign import ccall "ncurses.h wget_wch"
   c_wget_wch_ :: Ptr Win_t -> Ptr CInt -> IO CInt
 
--- | A thin wrapper around 'c_wget_wch_'; gives you a pair, the first
--- first of which is  a return code and the second of which is a char.
-c_wget_wch :: Ptr Win_t -> IO (CInt, CInt)
-c_wget_wch w = alloca $ \p -> (,) <$> c_wget_wch_ w p <*> peek p
+-- | A thin wrapper around 'c_wget_wch_'; gives you either a CInt
+-- representing some keypress or a Char.
+c_wget_wch :: Ptr Win_t -> IO (Maybe (Either CInt Char))
+c_wget_wch w = alloca $ \p -> do
+  -- The return code.
+  r <- c_wget_wch_ w p
+  -- The wide char or key code.
+  c <- peek p
+  -- If it's key_code_yes, we got a key code.
+  return $ if r == c_key_code_yes then Just $ Left c
+    -- If it's ok, then we got a character.
+    else if r == c_ok then Just . Right . chr . fromIntegral $ c
+    else Nothing
