98d2badb4436f4b992562eca2e128247dc1f2e3c
startling <tdixon51793@gmail.com>
2012-11-15 01:11:03 -0500

    deleted everything, got a new idea

--- UI/Curslet.hs
+++
@@ -1,3 +0,0 @@
-module UI.Curslet
-  ( module UI.Curslet.Class ) where
-import UI.Curslet.Class
--- example.hs
+++
@@ -1,8 +0,0 @@
-import UI.Curslet
-import UI.Curslet.Ncurses
-
-main = monochrome $ do
-  refresh $ do
-    attrs [bold] $ put "hello, world"
-    attrs [blink] $ addch '!'
-  getch
