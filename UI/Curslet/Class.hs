6abadd471364e88f681c9c1ae0010831a4d965e1
startling <tdixon51793@gmail.com>
2012-11-15 05:41:10 -0500

    moved the attribute stuff.

--- UI/Curslet/Class.hs
+++ UI/Curslet/Class.hs
@@ -1,7 +1,8 @@
-module UI.Curslet.Attributes where
+module UI.Curslet.Class where
 
 -- | A class that lets us switch some bits on some structure.
 class Style s where
   bold      :: Functor f => (Bool -> f Bool) -> s -> f s
   blink     :: Functor f => (Bool -> f Bool) -> s -> f s
   underline :: Functor f => (Bool -> f Bool) -> s -> f s
+
